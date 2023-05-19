8c2be84c3c36fdf47d79ab288f2eccd4ccfab176
vadim <cvi-vadim@yandex.ru>
2023-05-19 20:11:31 +0300

    fix

--- src/components/Page/JobPage/JobPage.jsx
+++ src/components/Page/JobPage/JobPage.jsx
@@ -18,9 +18,9 @@ const JobPage = () => {
         <Loader />
       ) : (
         <div>
-          {/* <JobCard {...data} /> */}
+          <JobCard {...data} />
 
-          {/* <div className={styles.Info}>{parse(data?.vacancyRichText)}</div> */}
+          <div className={styles.Info}>{parse(data?.vacancyRichText)}</div>
         </div>
       )}
     </div>
