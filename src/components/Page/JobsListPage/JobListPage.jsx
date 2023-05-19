9283c9df2a399b32f9d2cd1e85492d565f92cebf
vadim <cvi-vadim@yandex.ru>
2023-05-19 19:34:48 +0300

    fix

--- src/components/Page/JobsListPage/JobListPage.jsx
+++ src/components/Page/JobsListPage/JobListPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import style from "./JobListPage.module.css";
 import GroupFilter from "../../GroupFilter/GroupFilter";
 import JobList from "../../JobList/JobList";
